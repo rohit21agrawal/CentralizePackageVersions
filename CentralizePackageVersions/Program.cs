d652d18c8aa3e56b8eae65bcbf7501e38c55580b
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-12-05 16:32:44 -0800

    working prototype

--- CentralizePackageVersions/Program.cs
+++ CentralizePackageVersions/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections.Generic;
 
 namespace CentralizePackageVersions
 {
@@ -6,7 +10,108 @@ namespace CentralizePackageVersions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var files = Directory.GetFiles(@"E:\iotexp\Azure-IoT-ConnectedCar-Core\", "*.csproj", SearchOption.AllDirectories);
+            var dependencyFilePath = @"E:\iotexp\Azure-IoT-ConnectedCar-Core\infra\build\DependencyVersion.props";
+
+
+            var packageRefDictionary = new Dictionary<string, PackageReferenceModel>(StringComparer.OrdinalIgnoreCase);
+            foreach (var file in files)
+            {
+                using (var stream = new FileStream(file, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    try
+                    {
+                        var xml = XDocument.Load(stream);
+                        var ns = xml.Root.GetDefaultNamespace();
+                        var itemGroups = xml.Root.Elements().Where(t => t.Name.LocalName == "ItemGroup" && t.HasElements);
+
+                        foreach (var item in itemGroups)
+                        {
+                            var packageRefs = item.Elements(XName.Get("PackageReference", ns.NamespaceName));
+                            foreach (var reference in packageRefs)
+                            {
+                                if (reference.HasElements || reference.HasAttributes)
+                                {
+                                    ConvertElementsToAttributes(reference, ns.NamespaceName);
+                                    var name = reference.Attribute(XName.Get("Include")).Value;
+                                    var version = reference.Attribute(XName.Get("Version")).Value;
+
+                                    var referenceModel = GetPackageReferenceModel(name, version, packageRefDictionary);
+                                    reference.SetAttributeValue(XName.Get("Version"), $@"$({referenceModel.XmlEntry})");
+                                }
+                            }
+                        }
+                        ProjectFileUtils.WriteXmlToFile(xml, stream);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Exception thrown while trying to read file {file}. {e.Message}");
+                    }
+                }
+            }
+
+            UpdateDependencyVersionFile(dependencyFilePath, packageRefDictionary);
+
+            Console.ReadLine();
         }
+
+        private static void UpdateDependencyVersionFile(string filePath, Dictionary<string, PackageReferenceModel> packageRefDict)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                var xml = XDocument.Load(stream);
+                var ns = xml.Root.GetDefaultNamespace();
+                foreach (var dep in packageRefDict.Keys)
+                {
+                    ProjectFileUtils.AddProperty(xml, packageRefDict[dep].XmlEntry, packageRefDict[dep].Version);
+                }
+                ProjectFileUtils.WriteXmlToFile(xml, stream);
+            }
+        }
+
+        private static void ConvertElementsToAttributes(XElement element, string namespaceName)
+        {
+            var attributes = element.Elements();
+            var dictionary = new Dictionary<string, string>();
+            foreach (var attr in attributes)
+            {
+                var name = attr.Name.LocalName;
+                var value = attr.Value;
+                dictionary[name] = value;
+            }
+            element.RemoveNodes();
+
+            foreach (var newAttr in dictionary.Keys)
+            {
+                element.SetAttributeValue(XName.Get(newAttr), dictionary[newAttr]);
+            }
+        }
+
+        private static PackageReferenceModel GetPackageReferenceModel(string name, string version, Dictionary<string, PackageReferenceModel> packageRefDictionary)
+        {
+            if (packageRefDictionary.ContainsKey(name))
+            {
+                return packageRefDictionary[name];
+            }
+            else
+            {
+                var newName = name.Replace(".", "").Trim() + "Version";
+                packageRefDictionary[name] = new PackageReferenceModel()
+                {
+                    Name = name,
+                    XmlEntry = newName,
+                    Version = version
+                };
+
+                return packageRefDictionary[name];
+            }
+        }
+    }
+
+    public struct PackageReferenceModel
+    {
+        public string Name;
+        public string XmlEntry;
+        public string Version;
     }
 }
---
+++ CentralizePackageVersions/ProjectFileUtils.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CentralizePackageVersions
+{
+    class ProjectFileUtils
+    {
+        public static void WriteXmlToFile(XDocument xml, FileStream stream)
+        {
+            var unicodeEncoding = new UTF8Encoding();
+            var xmlString = xml.ToString();
+            stream.SetLength(0);
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Write(unicodeEncoding.GetBytes(xmlString), 0, unicodeEncoding.GetByteCount(xmlString));
+        }
+
+        private static void AddCondition(string condition, XElement subItem)
+        {
+            if (TrimAndGetNullForEmpty(condition) != null)
+            {
+                subItem.Add(new XAttribute(XName.Get("Condition"), condition));
+            }
+        }
+
+        public static void AddProperty(XDocument doc, string propertyName, string propertyValue)
+        {
+            AddProperty(doc, propertyName, propertyValue, condition: null);
+        }
+
+        public static void AddProperty(XDocument doc, string propertyName, string propertyValue, string condition)
+        {
+            var lastPropGroup = doc.Root.Elements().Last(e => e.Name.LocalName == "PropertyGroup");
+            var element = new XElement(XName.Get(propertyName), propertyValue);
+
+            AddCondition(condition, element);
+
+            lastPropGroup.Add(element);
+        }
+
+        public static string TrimAndGetNullForEmpty(string s)
+        {
+            if (s == null)
+            {
+                return null;
+            }
+
+            s = s.Trim();
+
+            return s.Length == 0 ? null : s;
+        }
+    }
+}
